f12fdbfc79236b6fc608e6311db12fb3b350d990
tonyxwz <tonyxwz@yahoo.com>
2021-02-14 23:20:51 +0100

    finished reading Effective C++

--- 4.LinkedList/2. Add Two Numbers.cc
+++ 4.LinkedList/2. Add Two Numbers.cc
@@ -12,24 +12,31 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution {
+class Solution
+{
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode *ans = nullptr;
-        ListNode** curr = &ans;
-        bool forward = false;
-        while(l1 && l2 || forward) {
-            int sum = (l1 ? l1->val : 0 ) + (l2 ? l2->val : 0);
-            if (forward) ++sum;
-            *curr = new ListNode(sum % 10);
-            forward = sum >= 10;
-            curr = &(*curr)->next;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
-        }
-        if (l1) *curr = l1;
-        if (l2) *curr = l2;
-        return ans;
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+  {
+    ListNode* ans = nullptr;
+    ListNode** curr = &ans;
+    bool forward = false;
+    while (l1 && l2 || forward) {
+      int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0);
+      if (forward)
+        ++sum;
+      *curr = new ListNode(sum % 10);
+      forward = sum >= 10;
+      curr = &(*curr)->next;
+      if (l1)
+        l1 = l1->next;
+      if (l2)
+        l2 = l2->next;
     }
+    if (l1)
+      *curr = l1;
+    if (l2)
+      *curr = l2;
+    return ans;
+  }
 };
 // ```
